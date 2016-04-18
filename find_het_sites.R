f2c2f310706f0cb2da185e6d163b159f4a484afb
diogro <diogro@gmail.com>
2016-04-18 12:36:10 -0300

    corrections for het location

--- find_het_sites.R
+++ find_het_sites.R
@@ -3,9 +3,24 @@ source("./find_private_aleles.R")
 by_line = laply(strsplit(colnames(snp_array), "_"), `[`, 1)
 get_heterozygote = function(x) tapply(x, by_line, function(row) length(unique(row))) - 1
 
-is_heterozygote = ldply(snp_array_list,
-                        function(snp_array) aaply(snp_array, 1, get_heterozygote),
-                        .parallel = TRUE)[,-1]
+is_heterozygote = tbl_df(ldply(snp_array_list,
+                               function(snp_array) aaply(snp_array, 1, get_heterozygote),
+                               .parallel = TRUE))
 names(is_heterozygote) <- paste(unique(by_line), "hets", sep = "_")
 save(is_heterozygote, file = "./data/heterozygotes_position.Rdata")
 #load("./data/heterozygotes_position.Rdata")
+
+just_snps <- bind_cols(just_snps, is_heterozygote)
+
+just_hets <- just_snps %>% select(CHROM, POS, contains("_hets"))
+just_hets
+
+library(RcppRoll)
+current_chr = "chr1"
+mean_het <-
+  just_hets %>%
+  filter(CHROM == current_chr) %>%
+  select(contains("_hets")) %>%
+  {roll_mean(as.matrix(.), 100001,
+             fill = NA, align = "center")}
+names(mean_het) <- paste(unique(by_line), "mean_het", sep = "_")
--- read_raw_snp_data.R
+++ read_raw_snp_data.R
@@ -14,9 +14,9 @@ raw_data$rows <- NULL
 line_order = c("A13", "A31", "A41", "A23", "A22", "A42")[6:1]
 
 just_snps = raw_data %>%
-            select(CHROM, POS, A22_hets, A13_1:A42_2) %>%
+            select(CHROM, POS, A13_1:A42_2) %>%
             filter(!grepl("_", CHROM)) %>%
             filter(!grepl("chrM", CHROM))
 
-snp_array = as.matrix(just_snps[,4:15])
-snp_array_list = dlply(just_snps, .(CHROM), function(x) as.matrix(x[,4:15]))
+snp_array = as.matrix(just_snps[,3:14])
+snp_array_list = dlply(just_snps, .(CHROM), function(x) as.matrix(x[,3:14]))
