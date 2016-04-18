af56a0c9e1a37e7844645f0d945afa08048a6530
diogro <diogro@gmail.com>
2016-04-18 10:24:25 -0300

    faster het search and proper output

--- find_het_sites.R
+++ find_het_sites.R
@@ -1,9 +1,11 @@
 source("./find_private_aleles.R")
 
 by_line = laply(strsplit(colnames(snp_array), "_"), `[`, 1)
-get_heterozygote = function(x) laply(tapply(x, by_line, unique), length) - 1
+get_heterozygote = function(x) tapply(x, by_line, function(row) length(unique(row))) - 1
 
-is_heterozygote = aaply(snp_array, 1, get_heterozygote)
-colnames(is_heterozygote) <- paste(unique(by_line), "hets", sep = "_")
+is_heterozygote = ldply(snp_array_list,
+                        function(snp_array) aaply(snp_array, 1, get_heterozygote),
+                        .parallel = TRUE)[,-1]
+names(is_heterozygote) <- paste(unique(by_line), "hets", sep = "_")
 save(is_heterozygote, file = "./data/heterozygotes_position.Rdata")
-load("./data/heterozygotes_position.Rdata")
+#load("./data/heterozygotes_position.Rdata")
