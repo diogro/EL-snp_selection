0e5454ce06ec85c578de7e58b4c3fb1f9be935f0
diogro <diogro@gmail.com>
2016-05-09 12:03:13 -0300

    good snps selection

--- chooseSnps.R
+++ chooseSnps.R
@@ -1,6 +1,8 @@
-source("./find_private_aleles.R")
+source("./find_all_aleles.R")
 
-chrom_sizes = all_snps %>%
+all_poly = all_poly %>% arrange(CHROM, POS)
+
+chrom_sizes = all_poly %>%
     group_by(CHROM) %>%
     summarize(start = min(POS), stop = max(POS), diff = (stop - start), min(QUAL) )
 chrom_sizes %>%  print(n = 21)
@@ -11,8 +13,9 @@ total_snps = 50000
 snps_per_chrom = floor(as.numeric(chrom_sizes$diff[-21]) * (total_snps/total_size))
 names(snps_per_chrom) = chrom_sizes$CHROM[-21]
 
-chunks_per_chrom = floor(snps_per_chrom / 12)
+chunks_per_chrom = floor(snps_per_chrom / 7)
 names(chunks_per_chrom) = chrom_sizes$CHROM[-21]
+sum(chunks_per_chrom)
 
 findChunksStartStop = function(i){
     total = chrom_sizes$diff[i]
@@ -29,7 +32,7 @@ chunksStartStop = llply(1:20, findChunksStartStop)
 names(chunksStartStop) = chrom_sizes$CHROM[-21]
 
 classifyPosition = function(current_chr){
-    x = all_snps %>% filter(CHROM == current_chr) %>% select(POS)
+    x = all_poly %>% filter(CHROM == current_chr) %>% select(POS) %>% arrange(POS)
     positions = as.numeric(x$POS)
     css = chunksStartStop[[current_chr]]
     k = 1
@@ -41,89 +44,104 @@ classifyPosition = function(current_chr){
     return(data_frame(chunk = positions))
 }
 chunks = tbl_df(ldply(chrom_sizes$CHROM[-21], classifyPosition, .progress = "text"))
-all_snps_noY = filter(all_snps, CHROM != "chrY")
-all_snps_noY$CHUNKS = as.numeric(chunks$chunk)
-all_snps_list = dlply(all_snps_noY, .(CHROM, CHUNKS), identity)
-t(laply(all_snps_list, nrow))
+all_poly_noY = filter(all_poly, CHROM != "chrY")
+all_poly_noY$CHUNKS = as.numeric(chunks$chunk)
+all_poly_list = dlply(all_poly_noY, .(CHROM, CHUNKS), identity)
+t(laply(all_poly_list, nrow))
 
-mean_qual = ddply(all_snps_noY[,c("CHROM", "CHUNKS", names(all_snps_list[[1]])[grep(".GQ", names(all_snps_list[[1]]))])],
-      .(CHROM, CHUNKS), function(x) colMeans(x[,-c(1, 2)]))
-min_qual = ddply(mean_qual, .(CHROM, CHUNKS), function(x) min(x[,-c(1, 2)]))
-qual_plot = ggplot(min_qual, aes(CHUNKS, V1, group = CHROM, color= CHROM)) + geom_point()
-save_plot(paste0("./data/jpegs/qual_chunk.png"),
-          qual_plot,
-          base_height = 5, base_aspect_ratio = 2)
-
-is_good_p = function(snp){
-    private_mask = grepl(paste0(snp$p_line, ".GQ"), names(snp))
-    GQ_mask = grepl(".GQ", names(snp))
-    snp[,private_mask] > 30 & all(snp[,GQ_mask] > 10)
+is_good = function(snp, is_private = FALSE, private = c(30, 10), non_private = 20){
+    if(!is_private){
+        GQ_mask = grepl(".GQ", names(snp))
+        all(snp[,GQ_mask] > non_private)
+    } else{
+        private_mask = grepl(paste0(snp$snp_type, ".GQ"), names(snp))
+        GQ_mask = grepl(".GQ", names(snp))
+        snp[,private_mask] > private[1] & all(snp[,GQ_mask] > private[2])
+    }
 }
-is_good = function(snp){
-    GQ_mask = grepl(".GQ", names(snp))
-    all(snp[,GQ_mask] > 20)
-}
-selectSNPchunk = function(x){
-    available_privates = na.omit(unique(x$p_line))
-    psnp_df = rbind_all(llply(available_privates, function(current_line) {
-                        possible_snps = filter(x, p_line == current_line) %>% arrange(desc(QUAL))
-                        for(i in seq(nrow(possible_snps))){
-                            snp = possible_snps[i,]
-                            if(is_good_p(snp)){ break
-                            } else snp = NULL
-                        }
-                        snp
-                        }
-                  )
-             )
-    available_usefull = na.omit(unique(x$pu_line[!x$is_private]))
-    usnp_df = rbind_all(llply(available_usefull, function(current_line) {
-                        possible_snps = filter(x, pu_line == current_line) %>% arrange(desc(QUAL))
-                        for(i in seq(nrow(possible_snps))){
-                            snp = possible_snps[i,]
-                            if(is_good(snp)){ break
-                            } else snp = NULL
-                        }
-                        snp
-                        }
-                  )
-             )
-    rbind_list(psnp_df, usnp_df)
-}
-selectSNPchunk(x)
-new_all_snps_noY = (all_snps_noY)
-all_snps_list = dlply(new_all_snps_noY, .(CHROM, CHUNKS), identity)
-selected_snps = tbl_df(ldply(all_snps_list, selectSNPchunk, .parallel = TRUE))
 
-new_all_snps_noY = setdiff(new_all_snps_noY, selected_snps)
-all_snps_list = dlply(new_all_snps_noY, .(CHROM, CHUNKS), identity)
-selected_snps = bind_rows(selected_snps, tbl_df(ldply(all_snps_list, selectSNPchunk, .parallel = TRUE)))
+selectFromAvailable = function(x, available, snps, min_dist, snps_per_class, ...){
+    for(current_snp_type in available){
+        k = 0
+        possible_snps = filter(x, snp_type == current_snp_type) %>% arrange(desc(QUAL))
+        for(i in seq(nrow(possible_snps))){
+            if(is_good(possible_snps[i,], ...)){
+                current_pos = possible_snps[i,]$POS
+                dists = abs(current_pos - x$POS)
+                dists = dists[dists != 0]
+                if(any(dists < min_dist)) next
+                snps = rbind_list(snps, possible_snps[i,])
+                k = k+1
+            }
+            if(k > snps_per_class) break
+        }
+    }
+    snps
+}
 
-sel_usnp_count <- selected_snps %>% count(pu_line, CHROM)
-sel_usnp_count %>% spread(pu_line, n) %>% print(n = 21)
+selectSNPchunk = function(x, min_dist = 30, snps_per_class = 4, ...){
+    snps = NULL
+    available_privates = unique(filter(x, class == "private")$snp_type)
+    snps = selectFromAvailable(x, available_privates, snps, min_dist, snps_per_class, is_private = TRUE, ...)
+    available_two_four = unique(filter(x, class == "two_four")$snp_type)
+    snps = selectFromAvailable(x, available_two_four, snps, min_dist, snps_per_class, ...)
+    available_50_50 = unique(filter(x, class == "50_50")$snp_type)
+    snps = selectFromAvailable(x, available_50_50, snps, min_dist, snps_per_class, ...)
+    available_het = unique(filter(x, class == "private_het")$snp_type)
+    snps = selectFromAvailable(x, available_het, snps, min_dist, snps_per_class, ...)
+    snps
+}
+
+x = all_poly_list[["chr1.507"]]
+selectSNPchunk(x) %>% select(POS, QUAL, TYPE, class, snp_type)
+chromY = all_poly %>% filter(CHROM == "chrY")
+y_snps = selectSNPchunk(chromY, min_dist = 30, 100, private = c(5, 0), non_private = 5) %>% filter(class != "private_het")
+y_snps %>% select(POS, QUAL, TYPE, class, snp_type) 
+
+snp_dist =sort(diff(just_snps$POS))
+snp_dist = snp_dist[snp_dist > 0]
+quantile(snp_dist, seq(0, 1, 0.1))
+
+new_all_poly_noY = (all_poly_noY)
+all_poly_list = dlply(new_all_poly_noY, .(CHROM, CHUNKS), identity)
+selected_snps = tbl_df(ldply(all_poly_list, selectSNPchunk, .parallel = TRUE)) %>% arrange(CHROM, POS)
+
+selected_dist = diff(selected_snps$POS)
+quantile(selected_dist[selected_dist>0], seq(0, 1, 0.01))
+too_close = which(selected_dist == 1)
+selected_snps[c(too_close, too_close+1),]
+chunksStartStop[['chr8']]
+
+sel_usnp_count <- selected_snps %>% count(snp_type, CHROM)
+sel_usnp_count %>% spread(snp_type, n) %>% print(n = 21)
+sel_usnp_quality <- selected_snps %>% group_by(CHROM) %>% summarize(min(QUAL), median(QUAL))
 
 u_snps_plot_data <-
-  selected_snps %>%
-  select(CHROM, POS, pu_line) %>%
-  mutate(POS = POS/1e6)
+    selected_snps %>%
+    select(CHROM, POS, snp_type) %>%
+    mutate(POS = POS/1e6)
+
+snp_order = as.character(unique(sel_usnp_count$snp_type))
+snp_order = snp_order[order(nchar(snp_order))]
+u_snps_plot_data$snp_type = factor(u_snps_plot_data$snp_type, levels = snp_order)
 psnps_plots = llply(unique(selected_snps$CHROM),
                     function(current_chr)
                     {
-                      counts = filter(sel_usnp_count, CHROM == current_chr)
-                      u_line_order = as.character(unique(filter(sel_usnp_count, CHROM == current_chr)$pu_line))
-                      counts = counts$n[which(u_line_order == filter(sel_usnp_count,
-                                                                   CHROM == current_chr)$pu_line)]
-                      legend = paste(u_line_order, "N =", counts)
-                      private_alele_plot <-
-                        u_snps_plot_data %>%
-                        filter(CHROM == current_chr) %>%
-                        ggplot(aes(pu_line, POS, color = pu_line)) +
-                        geom_point(size = 0.3) + geom_point(size = 0.2, aes(0.5, POS)) +
-                        coord_flip() + labs(x = "Line", y = "Chromossomal Position (Mb)") +
-                        scale_color_discrete(labels = legend, name = "") +
-                        ggtitle(current_chr)
-                      save_plot(paste0("./data/jpegs/selected_usnps_", current_chr, ".png"),
-                                private_alele_plot,
-                                base_height = 5, base_aspect_ratio = 2)
-                      return(private_alele_plot)
+                        counts = filter(sel_usnp_count, CHROM == current_chr)
+                        u_line_order = as.character(unique(filter(sel_usnp_count, CHROM == current_chr)$snp_type))
+                        u_line_order = u_line_order[order(nchar(u_line_order))]
+                        counts = counts$n[match(u_line_order, counts$snp_type)]
+                        legend = paste(u_line_order, "N =", counts)
+                        private_alele_plot <-
+                            u_snps_plot_data %>%
+                            filter(CHROM == current_chr) %>%
+                            ggplot(aes(snp_type, POS, color = snp_type)) +
+                            geom_point(size = 0.2) + geom_point(size = 0.2, aes(0.5, POS)) +
+                            coord_flip() + labs(x = "Line", y = "Chromossomal Position (Mb)") +
+                            scale_color_discrete(labels = legend, name = "") +
+                            ggtitle(current_chr)
+                        save_plot(paste0("./data/jpegs/selected_usnps_", current_chr, ".png"),
+                                  private_alele_plot,
+                                  base_height = 6.5, base_aspect_ratio = 2.3)
+                        return(private_alele_plot)
                     }, .parallel = TRUE)
